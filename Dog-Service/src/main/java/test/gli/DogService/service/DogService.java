0db3cfa9aff755a35547b1ef94a5976541cc4db4
Zadid Taqwa <Zadidtaqwa123@gmail.com>
2023-07-24 21:46:26 +0700

    remove .idea

--- Dog-Service/src/main/java/test/gli/DogService/service/DogService.java
+++ Dog-Service/src/main/java/test/gli/DogService/service/DogService.java
@@ -16,7 +16,6 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
